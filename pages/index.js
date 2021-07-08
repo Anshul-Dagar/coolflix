5198c23afd5ac10ddcab13132459a3bd12d9a0bb
Anshul <anshuldagar9@gmail.com>
2021-07-08 18:09:29 +0530

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import requests from '../utils/requests'
 
 
 export default function Home({results}) {
+  console.log(results);
   
   return (
     <div>
@@ -29,7 +30,7 @@ export async function getServerSideProps(context) {
   const genre = context.query.genre;
 
   const request = await fetch(
-    `https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending}`
+    `https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`
 
     ).then((res) =>res.json());
 
